77f04032942956a7ead14a3c221c3c568e94b03b
IanDLacy <Ian.D.Lacy@Gmail.com>
2021-01-27 00:48:19 +0000

    Automatic Commit

---
+++ Servers/PHP/index.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>Significant Brothers</title>
+<style>
+body
+{
+background-color:#000000;
+padding:32px;
+font-family:monospace;
+color:#FF7F00;
+text-align:center;
+}
+</style>
+</head>
+<body>
+<h1>It works!</h1>
+</body>
+</html>
