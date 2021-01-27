ec8aa70b1af2e1966341894fd50051d0ef621aec
IanDLacy <Ian.D.Lacy@Gmail.com>
2021-01-27 10:34:50 +0000

    Populated The PHP Server Added Basic Flesh To Root Page Created Directory For Resource Packs Moved Resource Pack Clarity Into The System Changed Server Resource Pack Back To Clarity

--- Servers/PHP/index.php
+++ Servers/PHP/index.php
@@ -1,18 +1,107 @@
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Significant Brothers</title>
 <style>
 body
 {
 background-color:#000000;
-padding:32px;
 font-family:monospace;
+font-size:16px;
 color:#FF7F00;
 text-align:center;
+}
+h2, ul
+{
+text-align:left;
+}
+a
+{
+overflow-wrap:break-word;
+color:#FF7F00;
+}
+a:hover
+{
+color:#FFFFFF;
 }
 </style>
 </head>
 <body>
-<h1>It works!</h1>
+<h1>The Significant Brothers' Minecraft Server</h1>
+<h2 style="text-align:center;">Minecraft is for the bois!</h2>
+<h2>Status</h2>
+<ul>
+<li>
+Online!
+</li>
+</ul>
+<h2>Whitelist</h2>
+<ul>
+<li>Ian [ Operator , Level 4 ]</li>
+<li>Jimmy</li>
+<li>Danny</li>
+<li>Sam</li>
+</ul>
+<h2>GitHub Repository</h2>
+<ul>
+<li>
+<a href="https://github.com/IanDLacy/Minecraft/" target="_blank">
+https://github.com/IanDLacy/Minecraft/
+</a>
+</li>
+</ul>
+<h2>Change Log</h2>
+<ul>
+<li>
+<a href="https://github.com/IanDLacy/Minecraft/commits/master/" target="_blank">
+https://github.com/IanDLacy/Minecraft/commits/master/
+</a>
+</li>
+</ul>
+<h2>Resource Packs</h2>
+<ul>
+<li>
+Clarity [ Currently Serving ] 
+	<ul>
+	<li>
+	<a href="https://www.curseforge.com/minecraft/texture-packs/clarity/" target="_blank">
+	Details
+	</a>
+	</li>
+	<li>
+	<a href="http://35.194.71.234/ResourcePacks/Clarity-1.16.zip" target="_blank">
+	Download
+	</a>
+	</li>
+	</ul>
+</li>
+<li>
+Jolicraft
+	<ul>
+	<li>
+	<a href="http://www.jolicraft.com/" target="_blank">
+	Details
+	</a>
+	</li>
+	<li>
+	<a href="http://35.194.71.234/ResourcePacks/jolicraft.zip" target="_blank">
+	Download
+	</a>
+	</li>
+	</ul>
+</li>
+</ul>
+<h2>Upcoming</h2>
+<ul>
+<li>
+Dynamic DNS
+</li>
+<li>
+Discord Bot
+</li>
+</ul>
+<h2 style="text-align:center;">
+Thank you for playing with me!
+</h2>
 </body>
 </html>
