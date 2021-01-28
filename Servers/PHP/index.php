cb9ef99d279638e1482f4e323b4426bd522692bc
IanDLacy <Ian.D.Lacy@Gmail.com>
2021-01-28 07:20:46 +0000

    Tidied Up The Server Index Page All Resource Packs Were Uploaded Had To Delete The Resource Packs In Order To Stay On GitHub Preparing For Migration To New Virtual Machine

--- Servers/PHP/index.php
+++ Servers/PHP/index.php
@@ -1,297 +1,280 @@
 <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Significant Brothers</title>
-<style>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Significant Brothers</title>
+		<style>
+
 body
 {
-background-color:#000000;
-font-family:monospace;
-font-size:16px;
-color:#FF7F00;
-text-align:center;
+	background-color : #000000 ;
+	font-family : monospace ;
+	font-size : 16px ;
+	color : #FF7F00 ;
+	text-align : center ;
 }
-h2, h3, ul
+
+h2 , h3 , ul
 {
-text-align:left;
+	text-align : left ;
 }
+
+h2
+{
+	padding-left : 16px ;
+}
+
+h3
+{
+	padding-left : 32px ;
+}
+
+h3 ~ ul
+{
+	padding-left : 64px ;
+}
+
+ul > li > ul
+{
+	padding-top : 8px ;
+	padding-bottom : 8px ;
+}
+
+ul > li > ul > li
+{
+	font-size : 14px ;
+}
+
 a
 {
-overflow-wrap:break-word;
-color:#FF7F00;
+	overflow-wrap : break-word ;
+	color : #FF7F00 ;
+	text-decoration : underline dotted ;
 }
+
 a:hover
 {
-color:#FFFFFF;
+	color : #FFFFFF ;
+	text-decoration : underline ;
 }
-</style>
-</head>
-<body>
-<h1>The Significant Brothers' Minecraft Server</h1>
-<h2 style="text-align:center;">Minecraft is for the bois!</h2>
-<h2>Status</h2>
-<ul>
-<li>
-Online!
-</li>
-</ul>
-<h2>Whitelist</h2>
-<ul>
-<li>Ian [ Operator , Level 4 ]</li>
-<li>Jimmy</li>
-<li>Danny</li>
-<li>Sam</li>
-</ul>
-<h2>Repository</h2>
-<ul>
-<li>
-<a href="https://github.com/IanDLacy/Minecraft/" target="_blank">
-https://github.com/IanDLacy/Minecraft/
-</a>
-</li>
-</ul>
-<h2>Change Log</h2>
-<ul>
-<li>
-<a href="https://github.com/IanDLacy/Minecraft/commits/master/" target="_blank">
-https://github.com/IanDLacy/Minecraft/commits/master/
-</a>
-</li>
-</ul>
-<h2>Coming Soon</h2>
-<ul>
-<li>
-Dynamic DNS
-</li>
-<li>
-Discord Bot
-</li>
-</ul>
-<h2>Resource Packs</h2>
-<h3>Currated From These Recent Recommendation Articles</h3>
-<ul>
-<li>
-<a href="https://www.pcgamer.com/best-minecraft-texture-packs/" target="_blank">https://www.pcgamer.com/best-minecraft-texture-packs/</a>
-</li>
-<li>
-<a href="https://www.pcgamesn.com/minecraft/15-best-minecraft-texture-packs/" target="_blank">https://www.pcgamesn.com/minecraft/15-best-minecraft-texture-packs/</a>
-</li>
-</ul>
-<h3>Details & Downloads</h3>
-<ul>
-<li>
-<a href="https://www.curseforge.com/minecraft/texture-packs/clarity/" target="_blank">Clarity</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Clarity-1.16.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="http://www.jolicraft.com/" target="_blank">Jolicraft</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/jolicraft.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.curseforge.com/minecraft/texture-packs/annahstas-beastrinia-pokemon-inspired-32x/" target="_blank">Annahstas Beastrinia 32x</a> ( Pokemon Inspired )
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Annahstas_Beastrinia_1.16_v.0.17.14.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://sapixcraft.com/" target="_blank">Sapixcraft</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 16x MC1.16.zip" target="_blank">
-	Download ( 16px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 32x MC1.16.zip" target="_blank">
-	Download ( 32px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 64x MC1.16.zip" target="_blank">
-	Download ( 64px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 128x MC1.16.zip" target="_blank">
-	Download ( 128px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 256x MC1.16.zip" target="_blank">
-	Download ( 256px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Sapixcraft 512x MC1.16.zip" target="_blank">
-	Download ( 512px )
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.curseforge.com/minecraft/texture-packs/bloom-bright-and-retro/" target="_blank">Bloom, Bright and Retro</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/bloom-1.16-1-26-2021.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.curseforge.com/minecraft/texture-packs/epic-adventures/" target="_blank">Epic Adventures</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Epic+Adventures.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.planetminecraft.com/texture-pack/retro-nes-revived/" target="_blank">Retro NES [Revived]</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/RetroNES_Rev 1.17.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.planetminecraft.com/texture-pack/fancy-3-0/" target="_blank">Fancy 3.5</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Fancy 3.5 1.16 128px.zip" target="_blank">
-	Download ( 128px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Fancy 3.5 1.16 512px.zip" target="_blank">
-	Download ( 512px )
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://resourcepack.net/mainly-realism-hd-resource-pack/" target="_blank">Mainly Photo Realism</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Mainly Photo Realism x128.zip" target="_blank">
-	Download ( 128px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Mainly Photo Realism x256.zip" target="_blank">
-	Download ( 256px )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/Mainly Photo Realism x512.zip" target="_blank">
-	Download ( 512px )
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.planetminecraft.com/texture-pack/pastel-craft-by-xradicald/" target="_blank">Pastel Craft by XradicalD</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/pastelcraft-release-10.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="http://www.chromahills.com/" target="_blank">Chroma Hills</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/ChromaHills-128x-1.16-v1.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://www.johnsmithlegacy.co.uk/index.php" target="_blank">John Smith Legacy</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/John Smith Legacy 1.16.4 v9.zip" target="_blank">
-	Download ( Standard | Classic )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/John Smith Legacy JSC 1.16.4 v9.zip" target="_blank">
-	Download ( JimStoneCraft Edition )
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://polyflore.net/projects/depixel/" target="_blank">Depixel</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Depixel.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://faithful.team/tag/faithful/" target="_blank">Faithful x32</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/Faithful_x32_1.16.zip" target="_blank">
-	Download
-	</a>
-	</li>
-	</ul>
-</li>
-<li>
-<a href="https://dokucraft.co.uk/" target="_blank">Dokucraft</a>
-	<ul>
-	<li>
-	<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-High.zip" target="_blank">
-	Download ( High )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-Dark.zip" target="_blank">
-	Download ( Dark )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-Light.zip" target="_blank">
-	Download ( Light )
-	</a>
-	</li>
-	<li>
-	<a href="/ResourcePacks/1.15-Dokucraft-Dwarven.zip" target="_blank">
-	Download ( Dwarven )
-	</a>
-	</li>
-	</ul>
-</li>
-</ul>
-<h2 style="text-align:center;">
-Thank you for playing with me!
-</h2>
-</body>
+
+		</style>
+	</head>
+	<body>
+
+		<h1>The Significant Brothers' Minecraft Server</h1>
+
+		<h2 style="text-align:center;">Minecraft is for the bois!</h2>
+
+		<h2>Status</h2>
+
+		<ul><li>Online!</li></ul>
+
+		<h2>Whitelist</h2>
+
+		<ul>
+			<li>Ian [ Operator , Level 4 ]</li>
+			<li>Jimmy</li>
+			<li>Danny</li>
+			<li>Sam</li>
+		</ul>
+
+		<h2>Repository</h2>
+
+		<ul>
+		<li>
+		<a href="https://github.com/IanDLacy/Minecraft/" target="_blank">https://github.com/IanDLacy/Minecraft/</a>
+		</li>
+		</ul>
+
+		<h2>Change Log</h2>
+
+		<ul>
+		<li>
+		<a href="https://github.com/IanDLacy/Minecraft/commits/master/" target="_blank">https://github.com/IanDLacy/Minecraft/commits/master/</a>
+		</li>
+		</ul>
+
+		<h2>Coming Soon</h2>
+
+		<ul>
+			<li>Dynamic DNS</li>
+			<li>Discord Bot</li>
+		</ul>
+
+		<h2>Resource Packs</h2>
+
+		<h3>Currated From These Recent Recommendation Articles</h3>
+
+		<ul>
+			<li>
+			<a href="https://www.pcgamer.com/best-minecraft-texture-packs/" target="_blank">https://www.pcgamer.com/best-minecraft-texture-packs/</a>
+			</li>
+			<li>
+			<a href="https://www.pcgamesn.com/minecraft/15-best-minecraft-texture-packs/" target="_blank">https://www.pcgamesn.com/minecraft/15-best-minecraft-texture-packs/</a>
+			</li>
+		</ul>
+
+		<h3>Details & Downloads</h3>
+
+		<ul>
+			<li>
+				<a href="https://www.curseforge.com/minecraft/texture-packs/clarity/" target="_blank">Clarity</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/Clarity-1.16.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="http://www.jolicraft.com/" target="_blank">Jolicraft</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/jolicraft.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.curseforge.com/minecraft/texture-packs/annahstas-beastrinia-pokemon-inspired-32x/" target="_blank">Annahstas Beastrinia</a> [ Pokemon Inspired ]
+				<ul>
+				<li>
+				<a href="/ResourcePacks/Annahstas_Beastrinia_1.16_v.0.17.14.zip" target="_blank">Download</a> [ 32 px ]
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://sapixcraft.com/" target="_blank">Sapixcraft</a>
+				<ul>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 16x MC1.16.zip" target="_blank">Download</a> [ &nbsp;16 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 32x MC1.16.zip" target="_blank">Download</a> [ &nbsp;32 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 64x MC1.16.zip" target="_blank">Download</a> [ &nbsp;64 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 128x MC1.16.zip" target="_blank">Download</a> [ 128 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 256x MC1.16.zip" target="_blank">Download</a> [ 256 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Sapixcraft 512x MC1.16.zip" target="_blank">Download</a> [ 512 px ]
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.curseforge.com/minecraft/texture-packs/bloom-bright-and-retro/" target="_blank">Bloom, Bright and Retro</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/bloom-1.16-1-26-2021.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.curseforge.com/minecraft/texture-packs/epic-adventures/" target="_blank">Epic Adventures</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/Epic+Adventures.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.planetminecraft.com/texture-pack/retro-nes-revived/" target="_blank">Retro NES</a> [ Revived ]
+				<ul>
+				<li>
+				<a href="/ResourcePacks/RetroNES_Rev 1.17.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.planetminecraft.com/texture-pack/fancy-3-0/" target="_blank">Fancy 3.5</a>
+				<ul>
+					<li>
+					<a href="/ResourcePacks/Fancy 3.5 1.16 128px.zip" target="_blank">Download</a> [ 128 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Fancy 3.5 1.16 512px.zip" target="_blank">Download</a> [ 512 px ]
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://resourcepack.net/mainly-realism-hd-resource-pack/" target="_blank">Mainly Photo Realism</a>
+				<ul>
+					<li>
+					<a href="/ResourcePacks/Mainly Photo Realism x128.zip" target="_blank">Download</a> [ 128 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Mainly Photo Realism x256.zip" target="_blank">Download</a> [ 256 px ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/Mainly Photo Realism x512.zip" target="_blank">Download</a> [ 512 px ]
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.planetminecraft.com/texture-pack/pastel-craft-by-xradicald/" target="_blank">Pastel Craft by XradicalD</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/pastelcraft-release-10.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="http://www.chromahills.com/" target="_blank">Chroma Hills</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/ChromaHills-128x-1.16-v1.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://www.johnsmithlegacy.co.uk/index.php" target="_blank">John Smith Legacy</a>
+				<ul>
+					<li>
+					<a href="/ResourcePacks/John Smith Legacy 1.16.4 v9.zip" target="_blank">Download</a> [ Standard | Classic ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/John Smith Legacy JSC 1.16.4 v9.zip" target="_blank">Download</a> [ JimStoneCraft Edition ]
+					</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://polyflore.net/projects/depixel/" target="_blank">Depixel</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/Depixel.zip" target="_blank">Download</a>
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://faithful.team/tag/faithful/" target="_blank">Faithful</a>
+				<ul>
+				<li>
+				<a href="/ResourcePacks/Faithful_x32_1.16.zip" target="_blank">Download</a> [ 32 px ]
+				</li>
+				</ul>
+			</li>
+			<li>
+				<a href="https://dokucraft.co.uk/" target="_blank">Dokucraft</a>
+				<ul>
+					<li>
+					<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-High.zip" target="_blank">Download</a> [ High ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-Dark.zip" target="_blank">Download</a> [ Dark ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/1.16.4-Dokucraft-TSC-Light.zip" target="_blank">Download</a> [ Light ]
+					</li>
+					<li>
+					<a href="/ResourcePacks/1.15-Dokucraft-Dwarven.zip" target="_blank">Download</a> [ Dwarven ]
+					</li>
+				</ul>
+			</li>
+		</ul>
+
+		<h2 style="text-align:center;">Thank you for playing with me!</h2>
+	</body>
 </html>
